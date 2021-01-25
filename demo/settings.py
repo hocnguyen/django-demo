987e24d6983685f327c67f3a33c7b7eb87d17425
Hoc Nguyen <hoc.nguyen@paradox.ai>
2021-01-25 15:35:37 +0700

    use coverall

--- demo/settings.py
+++ demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k((#3+!kjg!e8nh@5sic+)y-2+^_wmj073j*)m0_on($+*)%(o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- main/tests.py
+++ main/tests.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
 
 
-# Create your tests here.
+class AiAPITestBase(TestCase):
 
+    def test_first(self):
+        self.assertTrue(True)
+
+    def test_second(self):
+        self.assertTrue(False)
